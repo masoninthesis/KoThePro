4612c2e133c5a622ee87d5421f68ae3ea8908317
Mason L <mason.lawlor@me.com>
2014-11-13 01:32:38 -0800

    created site

--- base-front-page.php
+++ base-front-page.php
@@ -12,6 +12,91 @@
     get_template_part('templates/header');
   ?>
 
+  <div id="site-wrap">
+
+    <section id="hero">
+      <div class="wrap container">
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <h1>Quality · Service · Convenience</h1>
+            <a href="#" class="btn btn-primary btn-lg">Place Order</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="section1">
+      <div class="wrap container">
+        <div class="row">
+          <div class="col-sm-4">
+            <h3>Pickup & Deliver</h3>
+            <div id="box"></div>
+            <p>
+              Finder performance Downloads $399 change SSD A8X amazing. Hi-res thunderbolt Mavericks $599 screen. Better translucent Spotlight love better touch. Finder Smart Cover Cupertino $799 iTunes.
+            </p>
+            <h3>Pickup & Deliver</h3>
+            <div id="box"></div>
+            <p>
+              Finder performance Downloads $399 change SSD A8X amazing. Hi-res thunderbolt Mavericks $599 screen. Better translucent Spotlight love better touch. Finder Smart Cover Cupertino $799 iTunes.
+            </p>
+          </div>
+          <div class="col-sm-4 text-center">
+            <h3>
+              In by 10am, Ready by 4pm<br>
+              6 Days a Week<br>
+              Dry Cleaning and Laundry
+            </h3>
+            <h3 class="hours">Hours</h3>
+            <p>
+              Monday – Friday 7:00 am – 7:00 pm
+            </p>
+            <p>
+              Saturday 8:00 am – 6:00 pm
+            </p>
+            <p>
+              Sunday Closed
+            </p>
+          </div>
+          <div class="col-sm-4">
+            <h3>Location</h3>
+            <address>
+              <strong>Twitter, Inc.</strong><br>
+              795 Folsom Ave, Suite 600<br>
+              San Francisco, CA 94107<br>
+              <abbr title="Phone">P:</abbr> (123) 456-7890
+            </address>
+
+            <address>
+              <strong>Full Name</strong><br>
+              <a href="mailto:#">first.last@example.com</a>
+            </address>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <hr>
+
+    <section id="section2">
+      <div class="wrap container">
+        <div class="row">
+          <div class="col-sm-12">
+            <h3>Cupertino Retina Apple Zoom Yosemite</h3>
+            <p>
+              Download our free mobile app for iPhone and Android devices (including iPad and tablets) to instantly access great deals! It’s as easy as 1,2,3.
+            </p>
+            <ol>
+              <li>Apple Pay call iBooks Apple remote lock new features.</li>
+              <li>Apple Pay call iBooks Apple remote lock new features.</li>
+              <li>Apple Pay call iBooks Apple remote lock new features.</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </div>
+
   <div class="wrap container" role="document">
     <div class="content row">
       <main class="main" role="main">
--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,12 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+    <!-- <?php dynamic_sidebar('sidebar-footer'); ?> -->
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <p>
+          Copyright &copy; <?php echo date('Y'); ?> · My Website | all rights reserved.
+        </p>
+      </div>
+    </div>
   </div>
 </footer>
