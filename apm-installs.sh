ce3ba3825722a9411c3d1684681f54546ff4dd99
Mason L <mason.lawlor@me.com>
2014-11-12 21:36:12 -0800

    The Creation

---
+++ apm-installs.sh
@@ -0,0 +1,24 @@
+#Installs the Atom Terminal package
+apm install atom-terminal
+
+#Installs Term2 package
+apm install term2
+
+#Installs the Autocomplete Plus package
+apm install autocomplete-plus
+
+#Installs the Linter package
+#Linters for: html, css, less, javascript, php, and hemmingway proofreader
+apm install linter
+apm install linter-htmlhint
+apm install linter-csslint
+apm install linter-less
+apm install linter-jshint
+apm install linter-php
+apm install linter-write-good
+
+#Installs the Emmet package
+apm install emmet
+
+#Installs the Minimap package
+apm install minimap
